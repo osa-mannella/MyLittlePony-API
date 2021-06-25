659ad6f41184876b8ceee675983262b6484b6408
Era37 <osalorenzo@gmail.com>
2021-06-25 01:06:36 -0400

    Update api.py

--- api.py
+++ api.py
@@ -13,15 +13,34 @@ def character_get(name):
 
 @app.route("/")
 def home():
-    return "All information used belongs to the my little pony fandom (https://mlp.fandom.com/wiki/My_Little_Pony_Friendship_is_Magic_Wiki)"
+    return "Welcome to the My Little Pony make shift documentation!\n\n"
+
+@app.route("/places/")
+def _places():
+    q = []
+    for n in places:
+        q.append(n)
+    return jsonify(q)
+
+@app.route("/place/<place>/")
+def place(place):
+    for n in places:
+        if place == n:
+            return places[n]
+    return "No Result"
+
+@app.route("/episodes/")
+def _episodes():
+    q = []
+    for n in episodes:
+        q.append(n)
+    return jsonify(q)
 
 @app.route("/episode/<id>/")
 def episode(id):
     for n in episodes:
         if id == n:
             return episodes[id]
-        else:
-            continue
     return "No Result"
 
 @app.route("/character/<name>/")
